a724176f78e35e28c76af92c0b0af893bc316ada
kmaitreys <km.maitrey@gmail.com>
2025-05-29 18:03:07 +0530

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+//! `spectre` is intended to implement a subset of functionalities from `astropy`,
+//! `astroquery`, and `specutils` libraries. And perhaps some more for radio/sub-mm
+//! astronomy.
+//!
+//! Current plan is to implement the following:
+//!
+//! * `astroquery.linelists.cdms` - We will only implement readers, and not a full-fledged query tool.
+//! * `astroquery.lamda` - We will only implement readers, and not a full-fledged query tool.
+//! * `astroquery.hitran` - We will only implement readers, and not a full-fledged query tool.
+//! * `astroquery.jplspec` - We will only implement readers, and not a full-fledged query tool.
+//!
+//! * `specutils` - Likely full-fledged implementation.
+//! * `spectral_cube` - Likely full-fledged implementation.
+//! * `radio_beam` - Likely full-fledged implementation.
+
+pub mod beam;
+pub mod cdms;
+pub mod hitran;
+pub mod jpl;
+pub mod lamda;
+pub mod utils;
