ea9c8dc803c93471c70bdf55ee59c1f328624a07
kmaitreys <km.maitrey@gmail.com>
2025-07-04 23:49:34 +0530

    [ex] reorganize examples (thanks jblow)

--- examples/radio/01_jansky.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let l1 = f32::Length::new::<meter>(100.0);
-
-    println!(
-        "{} = {}",
-        l1.into_format_args(meter, Abbreviation),
-        l1.into_format_args(foot, Abbreviation)
-    );
-}
