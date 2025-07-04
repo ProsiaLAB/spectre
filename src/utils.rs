174a63b2be7e0fd2727674c964e19921f0d5e886
kmaitreys <km.maitrey@gmail.com>
2025-07-04 23:50:21 +0530

    [feat] new generic `approx_eq` for `uom` types

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,26 @@
-pub fn approx_eq(a: f64, b: f64, tol: f64) -> bool {
-    (a - b).abs() < tol
+use std::ops::Sub;
+
+use uom::si::f64::Angle;
+
+pub trait ApproxEq: Copy + PartialOrd + Sub<Output = Self> {
+    fn abs_diff(self, other: Self) -> Self;
+    fn approx_eq(self, other: Self, tolerance: Self) -> bool {
+        self.abs_diff(other) < tolerance
+    }
+}
+
+impl ApproxEq for f64 {
+    fn abs_diff(self, other: Self) -> Self {
+        (self - other).abs()
+    }
+}
+
+impl ApproxEq for Angle {
+    fn abs_diff(self, other: Self) -> Self {
+        (self - other).abs()
+    }
+}
+
+pub fn approx_eq<T: ApproxEq>(a: T, b: T, tolerance: T) -> bool {
+    a.approx_eq(b, tolerance)
 }
